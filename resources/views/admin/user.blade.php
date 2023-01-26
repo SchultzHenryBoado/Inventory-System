ac35fab689b3a5fe31f6276b7c781d5ecc6cdefb
unknown <schultzhenry.boado@obanana.com>
2023-01-26 00:58:21 -0800

    updated

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -9,15 +9,15 @@
 
   <!-- CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <!-- <link rel="stylesheet" href="//cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css"> -->
+  <link rel="stylesheet" href="//cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
 
   <!-- JS -->
-  <!-- <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script> -->
+  <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
   </script>
   <script src="https://kit.fontawesome.com/8cbc2e0f0e.js" crossorigin="anonymous"></script>
-  <!-- <script src="./js/tables.js" defer></script>
-  <script src="//cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js" defer></script> -->
+  <script src="{{ url('js/dataTable.js') }}" defer></script>
+  <script src="//cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js" defer></script>
 </head>
 
 <body>
@@ -176,7 +176,7 @@
 
     <!-- TABLE -->
     <div class="table-responsive">
-      <table class="table table-striped table-hover " id="dataTable" style="width: 100%;">
+      <table class="table table-striped table-hover " id="myTable" style="width: 100%;">
         <thead class="table-success">
           <tr>
             <th>Last name</th>
